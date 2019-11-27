04195b47beb6b6da429f396efbe0985c607027d7
rachel <mibrageh@naver.com>
2019-11-27 22:47:27 +0900

    리뷰 내용 메모

--- 3week-memoApp/memo-application/src/main.js
+++ 3week-memoApp/memo-application/src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+//root 시작
 new Vue({
   el: '#app',
-  store,
-  render: h => h(App)
+  store, //여기서 store에 있는 index.js를 기본으로 불러온다.
+  render: h => h(App) //App.vue를 불러오겠다
 })
--- 3week-memoApp/memo-application/src/store/actions.js
+++ 3week-memoApp/memo-application/src/store/actions.js
@@ -8,10 +8,10 @@ const memoAPICore = axios.create({
 export function fetchMemos({ commit }){
     memoAPICore.get('/')
         .then(res => {
-            commit(FETCH_MEMOS, res.data);
+            commit(FETCH_MEMOS, res.data); //여기서의 commit 은 바로 상단에서 전달받은 commit. 상단 commit 은  context.commit 으로도 쓸수있다.
         })
 }
-export function addMemo({ commit }, payload){
+export function addMemo({ commit }, payload){ //payload에는 전달받은 객체가 들어있음.
     memoAPICore.post('/', payload)
       .then(res => {
         commit(ADD_MEMO, res.data);  //API 응답 내의 메모 데이터를 commit 메소드를 통해 변이시킨다. 
