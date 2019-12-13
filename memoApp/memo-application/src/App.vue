52863d0f7bc7b38163facf61d486504085fb3655
bella <mibrageh@gmail.com>
2019-12-13 21:09:36 +0900

    메모앱 재작업 1차 커밋

---
+++ memoApp/memo-application/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <memo-App></memo-App>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader';
+import MemoApp from './components/MemoApp';
+
+export default {
+  name: 'app',
+  components: {
+    AppHeader,
+    MemoApp
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+@import "./styles/reset.css";
+
+#app {
+  width: 560px;
+  margin:0 auto;
+}
+
+</style>
---
+++ memoApp/memo-application/src/components/MemoApp.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="memo-app">
+        <memo-form></memo-form>
+        <ul class="memo-list">
+            <memo></memo>
+        </ul>
+    </div>
+    
+</template>
+
+<script>
+import MemoForm from './MemoForm';
+import Memo from './Memo';
+
+export default{
+    name:'MemoApp',
+    components: {
+        MemoForm,
+        Memo
+    },
+    data(){
+        return{
+            memos:[]
+        }
+    },
+    methods:{
+        addMemo(payload){
+            this.memos.push(payload);
+            alert(this.memos);
+        }
+    }
+}
+</script>
+<style scoped>
+  .memo-list {
+    padding: 20px 0;
+    margin: 0;
+  }
+</style>
