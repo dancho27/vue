d343dde3c3a45c53c5f0ef91a5d8a85132fc150d
bella <mibrageh@gmail.com>
2020-01-08 20:51:43 +0900

    커뮤니티앱 시작

---
+++ communityApp/board-example-frontend/src/components/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <table>
+      <colgroup>
+        <col style="width:10%;" />
+        <col style="width:60%;" />
+        <col style="width:10%;" />
+        <col style="width:20%;" />
+      </colgroup>
+      <thead>
+        <tr>
+          <th scope="col">번호</th>
+          <th scope="col">제목</th>
+          <th scope="col">작성자</th>
+          <th scope="col">작성일</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="post in posts" :key="post.id">
+          <th scope="col">{{ post.id }}</th>
+          <th scope="col">
+              <router-link :to="{ name: 'PostListPage' }">
+                  {{ post.title }}
+              </router-link>
+              [{{ post.comments.length }}]
+          </th>
+          <th scope="col">{{ post.user.name }}</th>
+          <th scope="col">{{ post.createdAt }}</th>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default{
+    name: 'PostList',
+    props: {
+        posts: {
+            type: Array
+        }
+    }
+}
+</script>
