7c41d4bcd83f46aae603dfc56945943523437a31
bella <mibrageh@gmail.com>
2020-01-16 20:59:28 +0900

    postViewpage 작업

--- communityApp/board-example-frontend/src/pages/PostViewPage.vue
+++ communityApp/board-example-frontend/src/pages/PostViewPage.vue
@@ -1,18 +1,43 @@
 <template>
     <div class="post-view-page">
-        <div class="post-view">
+        <!-- <div class="post-view">
             <div>
                 <h1>게시글 제목 노출</h1>
                 <span>게시판 번호 1</span>
                 <strong>홍길동 . 2019-01-01 09:00</strong>
             </div>
             <p>콘텐츠 내용 노출</p>
-        </div>
+        </div> -->
+        <post-view v-if="post" :post="post"/>
+        <p v-else>게시글 불러오는 중...</p>
         <router-link :to="{ name: 'PostListPage' }">목록</router-link>
     </div>
 </template>
 <script>
+import {  mapState, mapActions } from 'vuex'
+import PostView from '@/components/PostView'
+
 export default {
-    name: 'PostViewPage'
+    name: 'PostViewPage',
+    components: { PostView },
+    props:{
+        postId: {
+            type: String,
+            required: true
+        }
+    },
+    created(){
+        this.fetchPost(`/${this.postId}`)
+            .catch(err => {
+                alert(err.response.data.msg) //에러발생시 메세지 노출 후
+                this.$router.back() // 이전페이지로 보내준다.
+            })
+    },
+    methods: {
+        ...mapActions(['fetchPost'])
+    },
+    computed:{
+        ...mapState([ 'post' ])
+    }
 }
 </script>
--- communityApp/board-example-frontend/src/store/actions.js
+++ communityApp/board-example-frontend/src/store/actions.js
@@ -1,5 +1,5 @@
 /*
-- Actions의 주요 목적은 Mutations를 실행시키는 역활을 합니다. Mutations이 실행되면 state도 변경이 되겠지요.
+- Actions의 주요 목적은 Mutations를 실행시키는 역할을 합니다. Mutations이 실행되면 state도 변경이 되겠지요.
 - 동기 처리가 아니라 비동기처리를 합니다. 순서에 상관없이 먼저 종료된 함수의 피드백을 받아 후속 처리를 하게 됩니다.
 - dispatch('함수명', '전달인자')으로 실행 시킬 수 있습니다. ex) dispatch('함수명', '전달인자', {root:true})
 - actions 내에 함수 형태로 작성합니다.
@@ -7,13 +7,22 @@
 */
 
 import api from '@/api'
-import { FETCH_POST_LIST } from './mutations-types'
+import { 
+    FETCH_POST_LIST ,
+    FETCH_POST
+} from './mutations-types'
 
 export default{
     fetchPostList({ commit }){
         return api.get('/posts')
             .then(res => {
                 commit(FETCH_POST_LIST, res.data)
+            })
+    },
+    fetchPost({ commit }, postId ){
+        return api.get(`/posts/${postId}`)
+            .then(res => {
+                commit(FETCH_POST, res.data)
             })
     }
 }
