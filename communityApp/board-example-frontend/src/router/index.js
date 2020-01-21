5ebba2a36718624ccd6bbec0df6d36a804da67b5
bella <mibrageh@gmail.com>
2020-01-21 20:53:19 +0900

    회원가입 및 로그인 추가

--- communityApp/board-example-frontend/src/router/index.js
+++ communityApp/board-example-frontend/src/router/index.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import PostListPage from '@/pages/PostListPage'
 import PostViewPage from '@/pages/PostViewPage'
+import Signup from '@/pages/Signup'
+import Signin from '@/pages/Signin'
 
 Vue.use(Router)
 
+/*
+history mode? 
+vue-router의 기본 모드는 hash mode 입니다. 
+URL 해시를 사용하여 전체 URL을 시뮬레이트하므로 URL이 변경될 때 페이지가 다시 로드 되지 않습니다.
+해시를 제거하기 위해 라우터의 history 모드 를 사용할 수 있습니다. 
+history.pushState API를 활용하여 페이지를 다시 로드하지 않고도 URL 탐색을 할 수 있습니다.
+*/
 export default new Router({
   mode: 'history',
   routes: [
@@ -18,6 +27,16 @@ export default new Router({
       name: 'PostViewPage',
       component: PostViewPage,
       props: true
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
     }
   ]
 })
